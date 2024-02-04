f29d3934005cf0e309dcf0c4a898d4fcfd44e503
phonelaptop <jakeyjkim@gmail.com>
2024-02-04 10:12:16 -0800

    algos and algos

--- thirdLargest.js
+++ thirdLargest.js
@@ -1,4 +1,4 @@
-const secondLargest = (array) => {    
+const thirdLargest = (array) => {    
     // 1. find the largest number, remove largest number, fins the largest number
 
     // find the largest number 16 Math.max(...array)
@@ -8,19 +8,22 @@ const secondLargest = (array) => {
     const filtered = array.filter((num) => num !== max);
     console.log(filtered)
     // find the largest number again 13
-    const secondLargest = (Math.max(...filtered));
-    console.log("secondLargest",secondLargest);
+    const max2 = (Math.max(...filtered));
+    const filtered2 = filtered.filter((num) => num !== max2)
+    //find the largest number again
+    const thirdLargest = (Math.max(...filtered2));
+    console.log("thirdLargest",thirdLargest);
 }
 
-const secondLargest2 = (array) => {
+const thirdLargest2 = (array) => {
     // 2. sort the array from great the least, then grab index 1
 
     // sort the array array1.sort((a, b) => b > a ? 1 : -1);
     const sorttted = array.sort((a, b) => b > a ? 1 : -1);
     console.log(sorttted)
     // then print our the index 1
-    console.log("SecondLargest:",  sorttted[1])
+    console.log("thirdLargest:",  sorttted[2])
 }
 
-secondLargest2([12,11,13,16,9,8]); // 13
-//secondLargest([12,11,13,16,9,8]); // 13
+thirdLargest2([12,11,13,16,9,8]); // 13
+//thirdLargest([12,11,13,16,9,8]); // 13
